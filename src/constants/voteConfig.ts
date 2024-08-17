cd92d847e00b2f157c3dde41786336112326b564
salmonco <xkqjsslsek80@naver.com>
2024-08-17 17:45:24 +0900

    chore: vote data

---
+++ src/constants/voteConfig.ts
@@ -0,0 +1,13 @@
+export const VOTE_DATA = {
+  voteName: "8월 회식",
+  selectedDates: [
+    "Wed Aug 21 2024",
+    "Thu Aug 22 2024",
+    "Fri Aug 23 2024",
+    "Sat Aug 24 2024",
+  ],
+  startTime: 9,
+  endTime: 22,
+  places: ["유메야", "카탈로그", "아주대"],
+  memberCnt: 6,
+};
